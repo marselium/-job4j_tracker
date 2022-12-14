2249a35119b8d5bd9f374548370462c7743f16a3
Marsel Yumagulov <marseliumagulov@yandex.ru>
2022-12-14 20:02:52 +0300

    1.3. Вызов метода с аргументами. [#166086 #371205]]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -10,6 +10,7 @@ public class Jukebox {
             System.out.println("Песня не найдена");
         }
     }
+
     public static void main(String[] args) {
         Jukebox fender = new Jukebox();
         fender.music(9);
