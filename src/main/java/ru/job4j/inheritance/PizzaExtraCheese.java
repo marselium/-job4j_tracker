0d61e40594a6c39f75be2c49ef0fbacefd3c4f16
Marsel Yumagulov <116545491+marselium@users.noreply.github.com>
2022-12-20 18:50:50 +0300

    Delete PizzaExtraCheese.java

--- src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.inheritance;
-
-public class PizzaExtraCheese extends Pizza{
-    private static final String Name = "extra cheese";
-
-    public name(String name){
-        super(name);
-    }
-}
