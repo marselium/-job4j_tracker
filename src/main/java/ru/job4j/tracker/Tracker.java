2b7a2b56b5cfa7b8e976f8b71697a0c2d686e863
Marsel Yumagulov <marseliumagulov@yandex.ru>
2022-12-29 22:06:55 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,23 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] item = new Item[size];
+        int temp = 0;
+        for (int i = 0; i < size; i++) {
+            Item rsl = items[i];
+            if (key.equals(items[i].getName())) {
+                item[temp] = rsl;
+                temp++;
+            }
+        }
+        return Arrays.copyOf(item, temp);
     }
 
     public Item findById(int id) {
