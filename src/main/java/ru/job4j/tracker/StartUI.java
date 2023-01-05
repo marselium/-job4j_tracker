f47faa0b06c27da3aa2dff6f2ff9a24de41c2c63
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-05 22:13:19 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,9 +4,8 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
+
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String createdformatr = formatter.format(item.getCreated());
-        System.out.println(createdformatr);
+        System.out.println(item);
     }
 }
