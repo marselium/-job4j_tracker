18bb74c3f32dd44861dd771b92c2e5fce5eb7329
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-09 22:02:12 +0300

    9. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private String name;
+    private int sizeOfEmpty = 10;
+    public String surname;
+    public static final String NEWVALUE = "";
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    public Broken() {
+    }
+}
