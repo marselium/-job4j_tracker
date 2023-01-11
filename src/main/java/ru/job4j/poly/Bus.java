5ddc7b3333a5472a9061dc482b72d0ad17b0740e
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-11 14:22:45 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public int refill(int price) {
+        return 0;
+    }
+}
