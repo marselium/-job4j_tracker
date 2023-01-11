6c406b01a244b0eb6eb13858b1580bcadf00d6b6
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-11 13:20:37 +0300

    4.1. Элементы интерфейса [#504948]

---
+++ src/main/java/ru/job4j/polymorph/FuncMsg.java
@@ -0,0 +1,8 @@
+package ru.job4j.polymorph;
+
+public class FuncMsg implements Func2 {
+    public static void main(String[] args) {
+        FuncMsg funcMsg = new FuncMsg();
+        funcMsg.funcMessage();
+    }
+}
---
+++ src/main/java/ru/job4j/polymorph/MainFunc.java
@@ -0,0 +1,5 @@
+package ru.job4j.polymorph;
+
+public class MainFunc implements Func2 {
+
+}
