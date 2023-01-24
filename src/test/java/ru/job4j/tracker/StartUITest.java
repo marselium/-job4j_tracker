f8fb237ec914bd0e9a9033017e45ff8b9d2f6762
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-24 17:21:51 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -150,7 +150,7 @@ public class StartUITest {
     }
 
     @Test
-    public void whenInvalidExit(){
+    public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"8", "0"}
