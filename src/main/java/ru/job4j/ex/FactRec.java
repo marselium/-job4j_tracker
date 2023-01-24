90bcc1558b9f3a48d773ce63098f83e2e3b4b5c8
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-24 18:28:43 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #384575]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,11 +2,10 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-       if (n > 1) {
-           return calc(n - 1) * n;
-       } else {
+       if (n == 0 || n == 1) {
            return 1;
        }
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
