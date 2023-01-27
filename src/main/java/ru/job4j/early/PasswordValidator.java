08d5b92e10737ad5f1f84d5ceb2c2bd6bef7329d
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-27 16:56:23 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,51 +3,6 @@ package ru.job4j.early;
 import static java.lang.Character.*;
 
 public class PasswordValidator {
-    public static boolean upperCase(String word) {
-        boolean rsl = false;
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isUpperCase(word.charAt(i))) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    public static boolean lowerCase(String word) {
-        boolean rsl = false;
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isLowerCase(word.charAt(i))) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    public static boolean isDigit(String word) {
-        boolean rsl = false;
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isDigit(word.charAt(i))) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    public static boolean isSpec(String word) {
-        boolean rsl = false;
-        for (int i = 0; i < word.length(); i++) {
-            int ascii = word.charAt(i);
-            if ((ascii >= 32 && ascii <= 47) || (ascii >= 58 && ascii <= 64)
-                    || (ascii >= 91 && ascii <= 96) || (ascii >= 123 && ascii <= 126)) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
 
     public static boolean isSubstring(String word) {
         String[] substrings = {"qwerty", "12345", "password", "admin", "user"};
@@ -63,26 +18,42 @@ public class PasswordValidator {
     }
 
     public static String validate(String password) {
+        boolean isDigit = false;
+        boolean upperCase = false;
+        boolean lowerCase = false;
+        boolean isSpec = false;
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        for (int i = 0; i < password.length(); i++) {
+        for (char symbol : password.toCharArray()) {
+            if (isDigit(symbol)) {
+                isDigit = true;
+            }
+            if (isUpperCase(symbol)) {
+                upperCase = true;
+            }
+            if (isLowerCase(symbol)) {
+                lowerCase = true;
+            }
+            if (!isDigit(symbol) && !isLetter(symbol)) {
+                isSpec = true;
+            }
+        }
 
-            if (!upperCase(password)) {
-                throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-            }
-            if (!lowerCase(password)) {
-                throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-            }
-            if (!isDigit(password)) {
-                throw new IllegalArgumentException(("Password should contain at least one figure"));
-            }
-            if (!isSpec(password)) {
-                throw new IllegalArgumentException("Password should contain at least one special symbol");
-            }
+        if (!upperCase) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!lowerCase) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!isDigit) {
+            throw new IllegalArgumentException(("Password should contain at least one figure"));
+        }
+        if (!isSpec) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
         if (isSubstring(password)) {
             throw new IllegalArgumentException(
