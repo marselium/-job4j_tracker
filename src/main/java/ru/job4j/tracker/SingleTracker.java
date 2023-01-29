9d77ed0d654a7be33deb9cd5bcdfc0a1706ac1c6
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-01-29 21:20:41 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+
+    private Tracker tracker = new Tracker();
+
+    private static SingleTracker instance = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item createAction(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+    public Item[] show() {
+        return tracker.findAll();
+    }
+
+    public boolean edit(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public Item delete(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+}
