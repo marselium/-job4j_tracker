3fb7d26b44beb886d47c226c40b4e8c6882d6fcf
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-02-13 17:53:19 +0300

    9. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class Tracker {
     private final List<Item> items = new ArrayList<>();
+    private int ids = 1;
 
     public Item add(Item item) {
+        item.setId(ids++);
         items.add(item);
         return item;
     }
@@ -25,24 +27,25 @@ public class Tracker {
         return arr;
     }
 
-    private Item indexOf(int id) {
-        Item rsl = null;
-        for (Item item : items) {
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < items.size(); index++) {
+            Item value = items.get(index);
+            if (value.getId() == id) {
+                rsl = index;
             }
         }
         return rsl;
     }
 
     public Item findById(int id) {
-        return indexOf(id);
+        int index = indexOf(id);
+        return index != -1 ? items.get(index) : null;
     }
 
     public boolean replace(int id, Item item) {
-        Item index = indexOf(id);
-        boolean rsl = index != null;
+        int index = indexOf(id);
+        boolean rsl = index != -1;
         if (rsl) {
             item.setId(id);
             items.remove(index);
@@ -52,8 +55,8 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
-        Item index = indexOf(id);
-        boolean rsl = index != null;
+        int index = indexOf(id);
+        boolean rsl = index != -1;
         if (rsl) {
             items.remove(index);
         }
