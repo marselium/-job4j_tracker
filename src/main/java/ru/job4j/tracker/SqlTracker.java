0a962b020d3d4242313401f0be9a11e17c0f5372
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-12-13 17:03:25 +0300

    2.3.6. Liquibase 1. Liquibase. Установка. [#504892]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -22,7 +22,7 @@ public class SqlTracker implements Store, AutoCloseable {
     }
 
     public void init() {
-        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("db/liquibase.properties")) {
             Properties config = new Properties();
             config.load(in);
             Class.forName(config.getProperty("driver-class-name"));
